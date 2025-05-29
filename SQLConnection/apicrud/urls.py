aaf6141db8fbece683d4b2f79e8aebb2f54f6784
Mathias Vera M <131044151+MathiasVera12@users.noreply.github.com>
2025-05-29 16:30:25 -0500

    Primer uso de SQL en Django

---
+++ SQLConnection/apicrud/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .api import PersonViewSet
+
+router = routers.DefaultRouter()
+
+router.register('api/persons', PersonViewSet, 'persons')
+
+urlpatterns = router.urls
+
+
